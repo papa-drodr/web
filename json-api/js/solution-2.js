92e93f03495e0b4021c4da0fb91bc607c1c30221
thisaccountisdiff <paapp1234@naver.com>
2025-11-18 10:39:50 +0900

    Update quotes URL and modify fetch response handling

--- json-api/js/solution-2.js
+++ json-api/js/solution-2.js
@@ -1,14 +1,10 @@
-
-let quotesURL = "http://dummyjson.com/quotes/";
-const random = Math.floor(Math.random() * 1455);  // 0 ~ 1454 사이의 수
-quotesURL = "https://dummyjson.com/quotes/" + random;
+const quotesURL = "https://my-json-server.typicode.com/khlee2016/web2025/comments/1";
 console.log(quotesURL);
 fetch(quotesURL)
-  .then(response => response.json())
-  .then(data => {
-    const result = document.querySelector("#result");
-    result.querySelector(".quote").innerHTML = data.quote;
-    result.querySelector(".author").innerHTML = ` - ${data.author}`;
-  })
-  .catch(error => console.log(error));
-  
+.then(response => response.json())
+.then(data => {
+const result = document.querySelector("#result");
+result.querySelector(".quote").innerHTML = data.body;
+result.querySelector(".author").innerHTML = ` - ${data.user}`;
+})
+.catch(error => console.log(error));
